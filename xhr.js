2646eb22523cec2e27afd4cf83a3ad5e4c127d23
unknown <pouryasoleimani@gmail.com>
2026-05-01 16:29:13 +0330

    AJAX

--- xhr.js
+++ xhr.js
@@ -2,21 +2,16 @@ const btn = document.getElementById('AJAX__BUTTON')
 
 const text = document.getElementById('AJAX__TEXT')
 
-function loadText() {
+function injectText() {
   let xhr = new XMLHttpRequest()
-
   console.log({ btn, text, xhr })
-
   xhr.open('GET', "test.txt", true)
-
   xhr.onload = function () {
     if (xhr.status == 200) {
       text.innerHTML = this.response
     }
   }
-
   xhr.send()
-
 }
 
-btn.addEventListener('click', loadText)
+btn.addEventListener('click', injectText)
