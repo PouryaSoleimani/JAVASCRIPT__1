54bd15ed7f9d272fc259eb7f783d7467980130e5
unknown <pouryasoleimani@gmail.com>
2026-05-01 16:20:39 +0330

    XHR

--- xhr.js
+++ xhr.js
@@ -1,3 +1,22 @@
-const xhr = new XMLHttpRequest()
+const btn = document.getElementById('AJAX__BUTTON')
 
-xhr.open('GET', "https://jsonplaceholder.typicode.com/todos", true)
+const text = document.getElementById('AJAX__TEXT')
+
+function loadText() {
+  let xhr = new XMLHttpRequest()
+
+  console.log({ btn, text, xhr })
+
+  xhr.open('GET', "test.txt", true)
+
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      text.innerHTML = this.response
+    }
+  }
+
+  xhr.send()
+
+}
+
+btn.addEventListener('click', loadText)
