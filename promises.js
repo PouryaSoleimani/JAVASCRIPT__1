d9cab96471fabbdec21ae3396915b8928890884e
unknown <pouryasoleimani@gmail.com>
2026-05-07 19:32:33 +0330

    PROMISES IN JS

--- promises.js
+++ promises.js
@@ -15,6 +15,7 @@ class PromisesPage {
 
   init() {
     this.firstPromise();
+    this.secondPromise();
   }
 
   firstPromise() {
@@ -31,6 +32,18 @@ class PromisesPage {
     _promise
       .then((response) => console.log(response))
       .catch((error) => console.log(error));
+  }
+
+  secondPromise() {
+    const isAvailable = true;
+    return new Promise((resolve, reject) => {
+      if (isAvailable) {
+        resolve();
+        console.log("✅ ITEM IS AVAILABLE");
+      } else {
+        reject("⛔ ITEM IS NOT AVAILABLE");
+      }
+    });
   }
 }
 
