6d8dcc41568228d2b74cf842323117631dba48d9
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-05-24 23:25:22 +0330

    HANDLE___DYNAMIC___HEIGHT

---
+++ indexedDB.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+  let request = window.indexedDB.open('contacts', 1)
+
+  request.onerror = () => {
+    console.log('DB ERROR')
+  }
+
+  request.onsuccess = () => {
+    console.log('DB SUCCESS')
+    const db = request.result
+    console.log('DB =>', db)
+  }
+
+  request.onupgradeneeded = (e) => {
+    let db = e.target.result
+    // CREATE DATABASE
+    let objectStore = db.createObjectStore('contacts', {
+      keypath: 'id',
+      autoIncrement: true,
+    })
+    // CREATE SCHEMA / CREATE OBJECT STORE
+    objectStore.createIndex('firstName', 'firstName', {
+      unique: false
+    })
+    // CREATE SCHEMA / CREATE OBJECT STORE
+    objectStore.createIndex('lastName', 'lastName', {
+      unique: false
+    })
+
+    console.log('✅ DATABASE CREATED SUCCESSFULLY')
+  }
+
+
+
+})
