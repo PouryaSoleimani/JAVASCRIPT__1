ec84142f8ff261b2a07a1d7afedbd94cad24f3a0
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-05-27 13:18:26 +0330

    INDEXED__DB

--- indexedDB.js
+++ indexedDB.js
@@ -1,105 +1,124 @@
 const firstNameInput = document.getElementById('firstname')
 const lastNameInput = document.getElementById('lastname')
+const form = document.querySelector('.form')
 const btn = document.getElementById('submit___btn')
 const toast = document.querySelector('.toast')
 const toastText = document.querySelector('.text')
 const toastIcon = document.querySelector('.icon')
-let db;
 
-// INDEXED DB
-window.addEventListener('load', () => {
-  let request = window.indexedDB.open('contacts', 1)
-
-  request.onerror = () => {
-    console.log('DB ERROR')
+class IndexedDbClass {
+  constructor() {
+    this.db;
+    this.request = window.indexedDB.open('contacts', 1)
+    this.initDB()
+    this.eventHandlers()
   }
 
-  request.onsuccess = () => {
-    console.log('DB SUCCESS')
-    const db = request.result
-    console.log('DB =>', db)
+  initDB() {
+    this.request.onerror = () => {
+      console.log('DB ERROR')
+    }
+
+    this.request.onsuccess = () => {
+      const db = this.request.result
+      console.log('✅ DB SUCCESS =>', db)
+    }
+
+    this.request.onupgradeneeded = (e) => {
+      this.db = e.target.result // -> REFERING TO THE INDEXDB
+
+      // CREATE DATABASE
+      let objectStore = this.db.createObjectStore('contacts', {
+        keypath: 'id',
+        autoIncrement: true,
+      })
+
+      // CREATE SCHEMA / CREATE OBJECT STORE
+      objectStore.createIndex('firstName', 'firstName', {
+        unique: false
+      })
+
+      // CREATE SCHEMA / CREATE OBJECT STORE
+      objectStore.createIndex('lastName', 'lastName', {
+        unique: false
+      })
+
+      console.log('✅ DATABASE CREATED SUCCESSFULLY')
+    }
   }
 
-  request.onupgradeneeded = (e) => {
-    db = e.target.result // -> REFERING TO THE INDEXDB
+  formSubmitHandler(event) {
+    event.preventDefault()
 
-    // CREATE DATABASE
-    let objectStore = db.createObjectStore('contacts', {
-      keypath: 'id',
-      autoIncrement: true,
+    const firstVal = firstNameInput.value
+    const lastVal = lastNameInput.value
+
+    //! ON ERROR
+    if (!firstVal || !lastVal) {
+      toastIcon.innerHTML = `<i class="ph-fill ph-seal-warning red"></i>`
+      toastText.textContent = 'Please Fill Out the Form'
+      toast.style.setProperty('right', "20px")
+      setTimeout(() => {
+        toast.style.setProperty('right', "-400px")
+      }, 1500);
+      return
+    }
+
+    //* ON SUCCESS
+    if (firstVal || lastVal) {
+
+      let newItem = {
+        firstName: firstVal,
+        lastName: lastVal
+      }
+
+      if (!this.db) {
+        console.error("DB IS UNDEFINED ... ")
+      }
+
+      let transaction = this.db.transaction(['contacts'], 'readwrite')
+
+      let objectStore = transaction.objectStore('contacts')
+
+      let request = objectStore.add(newItem)
+
+      request.onsuccess = () => {
+        firstNameInput.value = ''
+        lastNameInput.value = ''
+      }
+
+      transaction.oncomplete = () => {
+        console.log('✅ TRANSACTION COMPLETED')
+      }
+
+      transaction.onerror = () => {
+        console.log('🛑 ERROR ON TRANSACTION')
+      }
+
+      // TOAST HANDLER
+      toastIcon.innerHTML = `<i class="ph-fill ph-seal-check green"></i>`
+      toastText.textContent = 'Form Submitted Successfully.'
+      toast.style.setProperty('right', "20px")
+      setTimeout(() => {
+        toast.style.setProperty('right', "-400px")
+      }, 1500);
+      console.log({ firstVal, lastVal })
+    }
+
+
+  }
+
+  eventHandlers() {
+    btn.addEventListener('click', (e) => this.formSubmitHandler(e))
+    form.addEventListener('keydown', (e) => {
+      if (e.key == 'Enter') {
+        this.formSubmitHandler()
+      }
     })
-
-    // CREATE SCHEMA / CREATE OBJECT STORE
-    objectStore.createIndex('firstName', 'firstName', {
-      unique: false
-    })
-
-    // CREATE SCHEMA / CREATE OBJECT STORE
-    objectStore.createIndex('lastName', 'lastName', {
-      unique: false
-    })
-
-    console.log('✅ DATABASE CREATED SUCCESSFULLY')
   }
-
-})
-
-// FORM SUBMITHANDLER
-function formSubmitHandler(e) {
-  e.preventDefault()
-
-  const firstVal = firstNameInput.value
-  const lastVal = lastNameInput.value
-
-  // ON ERROR
-  if (!firstVal || !lastVal) {
-    toastIcon.innerHTML = `<i class="ph-fill ph-seal-warning red"></i>`
-    toastText.textContent = 'Please Fill Out the Form'
-    toast.style.setProperty('right', "20px")
-    setTimeout(() => {
-      toast.style.setProperty('right', "-400px")
-    }, 1500);
-    return
-  }
-
-  // ON SUCCESS
-  if (firstVal || lastVal) {
-    let newItem = {
-      firstName: firstVal,
-      lastName: lastVal
-    }
-    console.log('db =>', db)
-
-    let transaction = db.transaction(['contacts'], 'readwrite')
-
-    let objectStore = transaction.objectStore('contacts')
-
-    let request = objectStore.add(newItem)
-
-    request.onsuccess = () => {
-      firstNameInput.value = ''
-      lastNameInput.value = ''
-    }
-
-    transaction.oncomplete = () => {
-      console.log('✅ TRANSACTION COMPLETED')
-    }
-
-    transaction.onerror = () => {
-      console.log('🛑 ERROR ON TRANSACTION')
-    }
-
-    // TOAST HANDLER
-    toastIcon.innerHTML = `<i class="ph-fill ph-seal-check green"></i>`
-    toastText.textContent = 'Form Submitted Successfully.'
-    toast.style.setProperty('right', "20px")
-    setTimeout(() => {
-      toast.style.setProperty('right', "-400px")
-    }, 1500);
-    console.log({ firstVal, lastVal })
-  }
-
 
 }
 
-btn.addEventListener('click', formSubmitHandler)
+document.addEventListener('DOMContentLoaded', () => {
+  new IndexedDbClass()
+})
