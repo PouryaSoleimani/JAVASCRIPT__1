c8ea22a453553b6c239189b6cf3d7dde23f1ca31
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-05-27 12:52:47 +0330

    INDEXED__DB

--- indexedDB.js
+++ indexedDB.js
@@ -4,7 +4,7 @@ const btn = document.getElementById('submit___btn')
 const toast = document.querySelector('.toast')
 const toastText = document.querySelector('.text')
 const toastIcon = document.querySelector('.icon')
-
+let db;
 
 // INDEXED DB
 window.addEventListener('load', () => {
@@ -21,7 +21,7 @@ window.addEventListener('load', () => {
   }
 
   request.onupgradeneeded = (e) => {
-    let db = e.target.result // -> REFERING TO THE INDEXDB
+    db = e.target.result // -> REFERING TO THE INDEXDB
 
     // CREATE DATABASE
     let objectStore = db.createObjectStore('contacts', {
@@ -47,9 +47,11 @@ window.addEventListener('load', () => {
 // FORM SUBMITHANDLER
 function formSubmitHandler(e) {
   e.preventDefault()
+
   const firstVal = firstNameInput.value
   const lastVal = lastNameInput.value
 
+  // ON ERROR
   if (!firstVal || !lastVal) {
     toastIcon.innerHTML = `<i class="ph-fill ph-seal-warning red"></i>`
     toastText.textContent = 'Please Fill Out the Form'
@@ -60,17 +62,41 @@ function formSubmitHandler(e) {
     return
   }
 
+  // ON SUCCESS
   if (firstVal || lastVal) {
+    let newItem = {
+      firstName: firstVal,
+      lastName: lastVal
+    }
+    console.log('db =>', db)
+
+    let transaction = db.transaction(['contacts'], 'readwrite')
+
+    let objectStore = transaction.objectStore('contacts')
+
+    let request = objectStore.add(newItem)
+
+    request.onsuccess = () => {
+      firstNameInput.value = ''
+      lastNameInput.value = ''
+    }
+
+    transaction.oncomplete = () => {
+      console.log('✅ TRANSACTION COMPLETED')
+    }
+
+    transaction.onerror = () => {
+      console.log('🛑 ERROR ON TRANSACTION')
+    }
+
+    // TOAST HANDLER
     toastIcon.innerHTML = `<i class="ph-fill ph-seal-check green"></i>`
     toastText.textContent = 'Form Submitted Successfully.'
     toast.style.setProperty('right', "20px")
-    firstNameInput.value = ""
-    lastNameInput.value = ""
     setTimeout(() => {
       toast.style.setProperty('right', "-400px")
     }, 1500);
     console.log({ firstVal, lastVal })
-    return
   }
 
 
